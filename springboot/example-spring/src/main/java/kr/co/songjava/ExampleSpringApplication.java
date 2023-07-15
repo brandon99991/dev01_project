ec3a1282e268837c3315fb305a300b26b1a7249e
DESK01\desk01 <induomo@gmail.com>
2023-07-15 18:15:32 +0900

    외장 톰캣으로 구동하기 위한 변경

--- springboot/example-spring/src/main/java/kr/co/songjava/ExampleSpringApplication.java
+++ springboot/example-spring/src/main/java/kr/co/songjava/ExampleSpringApplication.java
@@ -2,12 +2,19 @@ package kr.co.songjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class ExampleSpringApplication {
+public class ExampleSpringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleSpringApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ExampleSpringApplication.class);
+	}	
+	
 }
